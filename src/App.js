30742f0df214b9b537a3b9faa004b28c481ce35a
Lucas <lucasdcifuentes@gmail.com>
2020-10-10 10:28:54 -0500

    two columns with title and divider

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import React from 'react';
+import styled, {ThemeProvider} from "styled-components";
+import theme from "./styles/global"
+import Divider from "./components/Divider"
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+const LeftContent = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 100vh;
+  width: 300px;
+  background-color: ${props => props.theme.background };
+  color: #b0bec5;
+`
+
+const RightContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  margin-left: 3rem;
+  height: 100vh;
+`
 
 function App() {
   return (
-      <h1>Resume</h1>
+    <ThemeProvider theme={theme} >
+      <Container>
+        <LeftContent>left content</LeftContent>
+        <RightContent>
+          <h1>Lucas Cifuentes</h1>
+          <Divider />
+        </RightContent>
+      </Container>
+      </ThemeProvider>
   );
 }
 
