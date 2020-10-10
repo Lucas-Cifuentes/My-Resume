378224b91506a621823f2a3ce2eec2ddf5daae4f
Lucas <lucasdcifuentes@gmail.com>
2020-10-10 10:54:35 -0500

    change color with props

--- src/components/Divider.js
+++ src/components/Divider.js
@@ -3,10 +3,10 @@ import styled from "styled-components";
 
 const DividerLine = styled.div`
   display: flex;
-  border-top: 1px solid ${props => props.theme.primary};
+  border-top: 1px solid ${props => props.props.secondary ? props.theme.secondary : props.theme.primary};
   width: 25%;
 `
 
-const Divider = () => <DividerLine />;
+const Divider = (props) => <DividerLine props={props} />;
 
 export default Divider
