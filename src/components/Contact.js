07a075aebcf5713259fcffcdb76486e5d6a53f01
Lucas <lucasdcifuentes@gmail.com>
2020-10-10 11:23:41 -0500

    Contact info

---
+++ src/components/Contact.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import styled from "styled-components"
+import Divider from "./Divider"
+
+const Title = styled.h1`
+  font-size: 20px;
+  font-weight: bold;
+  text-transform: uppercase;
+`
+
+const Item = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 0.5rem;
+`
+
+const Text = styled.span`
+  margin-left: 0.5rem;
+`
+
+const Contact = () => {
+  return (
+    <>
+      <Title>Contact</Title>
+      <Divider secondary />
+      <Item>
+      <i class="fas fa-mobile-alt"></i> <Text>+54 2236017890</Text>
+      </Item>
+      <Item>
+      <i class="fas fa-envelope"></i> <Text>lucasdcifuentes@gmail.com</Text>
+      </Item>
+      <Item>
+      <i class="fas fa-map-marker-alt"></i> <Text>Mar del Plata, Argentina</Text>
+      </Item>
+    </>
+  )
+}
+
+export default Contact
