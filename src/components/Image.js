e55c16a6caaea0d551d28ea96f11485b25841798
Lucas <lucasdcifuentes@gmail.com>
2020-10-10 11:24:19 -0500

    change margin to image component

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,10 +3,9 @@ import photo from "../assets/photo.jpg";
 import styled from "styled-components"
 
 const ImageStyled = styled.img`
-  margin-top: 1rem;
-  margin-bottom: 1rem;
   width: 8rem;
   border-radius: 50%;
+  margin: 1rem auto;
 `
 
 const Image = () => <ImageStyled src={photo} alt="photo"/>
