85e6df2ef8b6cb5d8c9be91dfe3d2eecee25d1e0
Lucas <lucasdcifuentes@gmail.com>
2020-10-12 19:56:24 -0500

    add description component

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,22 +1,17 @@
 import React from 'react'
-import styled from "styled-components"
 import Divider from "./Divider";
 import Title from "./Title"
-
-const DescriptionStyled = styled.p`
-  color: ${props => props.theme.text};
-  font-size: 1.5rem;
-`
+import Description from "./Description";
 
 const Education = () => {
   return (
     <>
     <h1>Education</h1>
     <Title text="Title" />
-    <DescriptionStyled>
+    <Description size="1.5rem" >
       Computer Technician (2012-2018), EEST N°3 (Mar del Plata, Argentina)
-    </DescriptionStyled>
-    <Divider />
+    </Description>
+    <Divider large/>
     </>
   )
 }
