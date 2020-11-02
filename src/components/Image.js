279e0140e83860b409295320b9615da64741f130
Lucas <lucasdcifuentes@gmail.com>
2020-11-02 04:36:37 -0300

    change photo

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,6 +6,7 @@ const ImageStyled = styled.img`
   width: 8rem;
   border-radius: 50%;
   margin: 1rem auto;
+  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.3);
 `
 
 const Image = () => <ImageStyled src={photo} alt="photo"/>
