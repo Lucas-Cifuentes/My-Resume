d42c9b2b3b13536af4f1f07f79f346cea779921e
Lucas <lucasdcifuentes@gmail.com>
2020-11-02 04:41:17 -0300

    add shadow to left-content

--- src/components/LeftContent.js
+++ src/components/LeftContent.js
@@ -13,6 +13,7 @@ const LeftContentStyed = styled.div`
   width: ${props => props.width < 600 ? "100%" : "18rem" };
   background-color: ${props => props.theme.background };
   color: ${props => props.theme.secondary};
+  box-shadow: 0px 0px 10px 5px rgba(0, 0, 0, 0.3);
 `
 
 const Title = styled.h2`
